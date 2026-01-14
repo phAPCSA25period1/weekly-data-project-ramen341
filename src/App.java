8ca104e092087785ffb66dbc6a50d18b00141587
ramen341 <rnraymond1@gmail.com>
2026-01-14 22:06:25 +0000

    Done

--- src/App.java
+++ src/App.java
@@ -10,12 +10,21 @@ import java.util.Scanner;
 public class App {
 
     public static void main(String[] args) {
-
+        Scanner input = new Scanner(System.in);
+        
         // -------------------------------------------------------------
         // TODO 1: Create a Scanner for user input
         // -------------------------------------------------------------
-
-
+        introduction();
+        inputBlocker(input);
+        WeeklyData sleepData = new WeeklyData(createSleepData(input));
+        inputBlocker(input);
+        System.out.println();
+        displayInsights(sleepData);
+        inputBlocker(input);
+        System.out.println(sleepData);
+        
+    
         // -------------------------------------------------------------
         // TODO 2: Give information about your program
         //         Ask the user about their goals (if applicable)
@@ -73,4 +82,91 @@ public class App {
 
 
     }
+    public static void introduction()
+    {
+        System.out.println("This program collects and analyzes your weekly data of sleep.");
+        System.out.println("You will be prompted to enter the number of hours you sleep each day for a week");
+        System.out.println("We take that data and analyze it in order to give you insights of your sleep");
+        System.out.println("Click any button to continute throughout the program!");
+    }
+
+    public static double[] createSleepData(Scanner input)
+    {
+        double[] sleepData = new double[7];
+        for (int i = 0; i < 7; i++)
+        {
+            try{
+                System.out.println("Enter the number of hours you slept for day " + (i + 1) + ": ");
+                double hours = input.nextDouble();
+                sleepData[i] = hours;
+            }
+            catch(Exception e)
+            {
+                System.out.println("Invalid input. Please enter a valid number.");
+                input.next(); 
+                i--; 
+            }
+            
+        }
+        return sleepData;
+    }
+
+    public static void displayInsights(WeeklyData data)
+    {
+        double averageSleep = data.getAverage();
+        System.out.printf("Your average sleep this week was: %.2f hours\n", averageSleep);
+        if (averageSleep < 8)
+        {
+            System.out.println("You need to get more sleep next week!");
+        }
+        else if (averageSleep >= 8 && averageSleep <= 10)
+        {
+            System.out.println("You had a healthy amount of sleep this week!");
+        }
+        else
+        {
+            System.out.println("You slept a lot this week! Make sure it's quality sleep.");
+        }
+        System.out.println();
+        System.out.printf("Your maximum sleep this week was: %.2f hours\n", data.getMax());
+        double maxSleep = data.getMax();
+        if (maxSleep > 10)
+        {
+            System.out.println("You had a day with excessive sleep! Try to maintain a consistent sleep schedule.");
+        }
+        else if(maxSleep >= 8 && maxSleep <= 10)
+        {
+            System.out.println("Your maximum sleep was within a healthy range. Get more of these!");
+        }
+        else
+        {
+            System.out.println("Consider getting more sleep on your busiest days.");
+        }
+        System.out.println();
+        System.out.println("Your minimum sleep this week was: " + data.getMin() + " hours");
+        double minimumSleep = data.getMin();
+            if (minimumSleep < 6)
+            {
+                System.out.println("You had a day with very little sleep! Try to avoid this consitency matters!");
+            }
+            else
+            {
+                System.out.println("Your minimum sleep was decent. Keep it up!");
+        }
+        System.out.println();
+        System.out.println("Your total sleep this week was: " + data.getTotal() + " hours");
+        double totalSleep = data.getTotal();
+        if (totalSleep < 56){
+            System.out.println("You need to increase your total sleep hours next week!");
+        }
+        else{
+            System.out.println("Great job on getting enough sleep this week!");
+        }
+
+    }
+
+    public static void inputBlocker(Scanner input){
+        input.nextLine();
+    }
 }
+
---
+++ src/WeeklyData.java
@@ -0,0 +1,60 @@
+public class WeeklyData {
+
+    
+    double[] sleep;
+
+    
+    public WeeklyData(double[] input) {
+        sleep = input;
+        
+    }
+
+
+    
+    public double getTotal() {
+        double total = 0.0;
+        for(int i=0; i < sleep.length; i++)
+            total += sleep[i];
+        return total; 
+    }
+
+
+    
+    public double getAverage() {
+        return (getTotal()/sleep.length); 
+    }
+
+
+  
+    public double getMax() {
+        double max = sleep[0];
+        for(int i=1; i < sleep.length; i++){
+            if(sleep[i] > max)
+                max = sleep[i];
+        }
+        return max; 
+    }
+
+
+    
+    public double getMin() {
+       double min = sleep[0];
+       for(int i=1; i < sleep.length; i++){
+            if(sleep[i] < min)
+                min = sleep[i];
+       }
+        return 0.0; 
+    }
+
+
+
+     
+    @Override
+    public String toString() {
+        String result = "\nWeekly Sleep Data:\n";
+        for(int i=0; i < sleep.length; i++){
+            result += ("Day " + (i+1) + ": " + sleep[i] + " hours\n");
+        }
+        return result;
+    }
+}
